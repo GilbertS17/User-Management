d5121993082651888f0809696dca35a928611c4b
gilbert sahyoun <gilbertsahyoun.business@gmail.com>
2025-04-11 12:38:20 +0300

    Feat: add private route page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router"
 import Dashboard from "./Components/pages/Dashboard"
 import NotFound from "./Components/pages/Notfound"
 import Login from "./Components/pages/Login"
+import PrivateRoute from "./Components/pages/PrivateRoute"
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
